8e6721c3addf2fea26ea35ca9fbd3a39c853c707
furqanbilal <furquanbazaz@gmail.com>
2020-06-28 15:01:39 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MaterialApp(
+    title: "STS",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Smart Transit"),
+        backgroundColor: Colors.amberAccent,
+      ),
+    ),
+  ));
+}
